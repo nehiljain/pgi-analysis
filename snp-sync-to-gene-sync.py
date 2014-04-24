990ad75d95b72c4260d50cbc6a05dd3dab108eae
nehiljain <jain.nehil@gmail.com>
2014-04-23 22:51:54 -0300

    some changes

--- snp-sync-to-gene-sync.py
+++ snp-sync-to-gene-sync.py
@@ -1,6 +1,6 @@
 import os
 
-fileObject = open("~/pgi-ngs-analysis/snp-to-gene-sync.sh", "wb")
+fileObject = open("/home/nehil/pgi-ngs-analysis/snp-to-gene-sync.sh", "wb")
 
 
 
@@ -9,7 +9,7 @@ fileObject.write("#!/bin/sh \n")
 for file in os.listdir("/share/volatile_scratch/nehil/sync_files"):
         if file.endswith(".sync"):
         		# fileObject.write("echo \" "+ str(file) + "\"\n\n") 
-                fileObject.write("perl -d:DProf ~/popoolation2_1201/create-genewise-sync.pl " + 
+                fileObject.write("perl -d:DProf /home/nehil/popoolation2_1201/create-genewise-sync.pl " + 
                 "--input /share/volatile_scratch/nehil/sync_files/" + file + 
                 " --gtf /share/volatile_scratch/nehil/sync_files/Mus_musculus.GRCm38.75.gtf" + 
                 " --output /share/volatile_scratch/nehil/gene-sync-files/" + os.path.splitext(file)[0] + ".sync  \n\n")
