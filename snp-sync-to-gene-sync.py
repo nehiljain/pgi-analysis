d0687b76f4ebbad7480c603cb075368f7678ade3
nehiljain <jain.nehil@gmail.com>
2014-04-23 22:49:42 -0300

    some changes

--- snp-sync-to-gene-sync.py
+++ snp-sync-to-gene-sync.py
@@ -8,6 +8,10 @@ fileObject.write("#!/bin/sh \n")
 
 for file in os.listdir("/share/volatile_scratch/nehil/sync_files"):
         if file.endswith(".sync"):
-        		fileObject.write("echo \" "+ str(file) + "\"\n\n") 
-                fileObject.write("perl -d:DProf ~/popoolation2_1201/create-genewise-sync.pl " + "--input /share/volatile_scratch/nehil/sync_files/" + file + " --gtf /share/volatile_scratch/nehil/sync_files/Mus_musculus.GRCm38.75.gtf" + " --output /share/volatile_scratch/nehil/gene-sync-files/" + os.path.splitext(file)[0] + ".sync  \n\n" )
+        		# fileObject.write("echo \" "+ str(file) + "\"\n\n") 
+                fileObject.write("perl -d:DProf ~/popoolation2_1201/create-genewise-sync.pl " + 
+                "--input /share/volatile_scratch/nehil/sync_files/" + file + 
+                " --gtf /share/volatile_scratch/nehil/sync_files/Mus_musculus.GRCm38.75.gtf" + 
+                " --output /share/volatile_scratch/nehil/gene-sync-files/" + os.path.splitext(file)[0] + ".sync  \n\n")
+
 fileObject.close()
