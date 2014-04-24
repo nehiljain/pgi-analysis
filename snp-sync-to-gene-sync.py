81d1380cf390cef917c2f5c8888566abb64e3ca5
nehiljain <jain.nehil@gmail.com>
2014-04-23 22:28:26 -0300

    sno to gene sync files

---
+++ snp-sync-to-gene-sync.py
@@ -0,0 +1,16 @@
+import os
+
+fileObject = open("~/pgi-ngs-analysis/snp-to-gene-sync.sh", "wb")
+
+
+
+fileObject.write("#!/bin/sh \n")
+
+for file in os.listdir("/share/volatile_scratch/nehil/sync_files"):
+        if file.endswith(".sync"):
+        		fileObject.write("echo \" "+ str(file) + "\"\n\n") 
+                fileObject.write("perl -d:DProf ~/popoolation2_1201/create-genewise-sync.pl " + 
+                	"--input /share/volatile_scratch/nehil/sync_files/" + file + 
+                	" --gtf /share/volatile_scratch/nehil/sync_files/Mus_musculus.GRCm38.75.gtf" + 
+                	" --output /share/volatile_scratch/nehil/gene-sync-files/" + os.path.splitext(file)[0] + ".sync  \n\n" )
+fileObject.close()
