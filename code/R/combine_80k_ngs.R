6fc7550241c6d67a114c20f0c26550084b92473b
nehiljain <jain.nehil@gmail.com>
2014-11-11 20:08:36 -0400

    gtf processing completed

--- code/R/combine_80k_ngs.R
+++ code/R/combine_80k_ngs.R
@@ -4,7 +4,30 @@ library(plyr)
 library(dplyr)
 library(ggplot2)
 library(stringr)
-library(XLConnect)
+library(xlsx)
+library(rattle)
+setwd("~/coderepo/pgi-ngs-analysis/data/combine_80k_ngs/")
+
+ngsData = read.xlsx2("ngs_top_genes.xlsx", sheetName = "Sheet1")
+names(ngsData) <- normVarNames(names(ngsData))
+
+microarrayData = read.csv("wgr_annotated_top_rank_genelist.csv")
+names(microarrayData) <- normVarNames(names(microarrayData))
+
+# change name of a column 
+colnames(microarrayData)[2] <- colnames(ngsData)[2]
+colnames(microarrayData)[5] <- colnames(ngsData)[1]
+
+combinedDataFrame <- merge(x = ngsData, y = microarrayData, by = "gene_id", all= FALSE)
+
+drops <- c("x", "x_1", "chromosome_no.y")
+
+combinedDataFrame <- combinedDataFrame[, !(names(combinedDataFrame) %in% drops )]
+notFoundGeneIds <- setdiff(microarrayData$gene_id, test1$gene_id)
+
+
+write.csv(combinedDataFrame, file="combined_80k_ngs_data.csv")
+write.csv(notFoundGeneIds, file="genes_not_found_in_ngs.csv")
 
 
 
---
+++ code/R/gtf-munging.R
@@ -0,0 +1,109 @@
+rm(list=ls())
+
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library(stringr)
+library(xlsx)
+library(rattle)
+
+# This script loads the GTF file for mouse, creates a header, munges the strings to be made useful, converts to a dataframe and writes it to RData and CSV Files
+# change the path to point to the GTF file from ensemble ftp://ftp.ensembl.org/pub/release-77/gtf/mus_musculus
+# previewDf gives us idea about the structure of the file
+# Header is derived from http://uswest.ensembl.org/info/website/upload/gff.html
+
+# change the path to point to the GTF file from ensemble ftp://ftp.ensembl.org/pub/release-77/gtf/mus_musculus
+
+# WARNING: No gene name found for line 21995 to 22008 in mouse GTF file path and url above. Replaced with "NA"
+gtfFilePath <- "Downloads/Mus_musculus.GRCm38.77.gtf"
+
+# previewDf <- read.table(file = gtfFilePath,
+#                         header = FALSE,
+#                         comment.char = "#",
+#                         nrow = 100,
+#                         na.strings = "NA",
+#                         fill = TRUE,
+#                         sep = "\t")
+
+
+headerName <- c("chromosome_name", "source", "feature", "start", "end", "score", "strand", "frame", "attribute")
+
+colClassNames <- c("character", "factor", "factor", "integer", "integer", "character", "character", "character", "character")
+gtfData <- read.table(file = gtfFilePath,
+                      header = FALSE,
+                      comment.char = "#",
+                      na.strings = "NA",
+                      fill = TRUE,
+                      sep = "\t",
+                      col.names = headerName)
+
+## Reading is complete.
+## Next we manipulate the attribute column to create 3 additional columns for gene id, gene name and gene biotype
+
+removeGeneNameTag <- function(row) {
+    
+    s <- as.character(row)
+#     print(s)
+    gene_name_loc <- str_locate(s, "gene_name ")
+    if (!is.na(gene_name_loc[1])) {
+        return(str_trim(str_sub(s, start = gene_name_loc[2])))  
+    }
+    #     print(s)
+    return(str_trim(s))
+    
+}
+
+removeGeneIdTag <- function(row) {
+    s <- as.character(row)
+    #     print(s)
+    gene_id_loc <- str_locate(s, "gene_id ")
+    #   print(gene_id_loc)
+    if (!is.na(gene_id_loc[1])) {
+        return(str_trim(str_sub(s, start = gene_id_loc[2])))  
+    }
+    
+    return(str_trim(row))
+    
+    
+}
+
+removeGeneBiotypeTag <- function(row) {
+    
+    s <- as.character(row)
+    #    print(s)
+    gene_biotype_loc<- str_locate(s, "gene_biotype ")
+    if (!is.na(gene_biotype_loc[1])) {
+        return(str_trim(str_sub(s, start = gene_biotype_loc[2])))  
+    }
+    return(str_trim(s))  
+}
+
+
+splitAttributes <- str_split(gtfData$attribute, "; ")
+
+formattedAttributes <- ldply(splitAttributes, function (row) { 
+    id <- removeGeneIdTag(row[grep("gene_id", row)])
+    if ( length(grep("gene_name", row)) == 0 ) {
+        g_name <- "NA"
+        # this is done for line 21995 to 22008 in mouse GTF file path and url above.
+    } else {
+        g_name <- removeGeneNameTag(row[grep("gene_name", row)])   
+    }
+    biotype <- removeGeneBiotypeTag(row[grep("gene_biotype", row)])
+    print(g_name)
+    df <- data.frame(gene_id = id, gene_name = g_name, gene_biotype = biotype)
+})
+
+resultGtfData <- cbind(gtfData, formattedAttributes)
+
+str(resultGtfData)
+
+save(resultGtfData, file = "mouse_gtf.RData")
+
+
+write.csv(resultGtfData, file = "mouse_gtf.RData", append = FALSE,
+          quote = FALSE, na = "NA", row.names = FALSE,
+          col.names = TRUE)
+
+
+
